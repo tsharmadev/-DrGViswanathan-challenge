b094a492d0c83805410c654c9614d539d17fe847
DevTanisha-max <stanisha843@gmail.com>
2026-08-02 00:23:01 +0530

    Day 48: Solved Check If It Is a Good Array & Hamming Distance

---
+++ Day-48/solution1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isGoodArray(int[] nums) {
+        int gcd = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            gcd = gcd(gcd, nums[i]);
+            if (gcd == 1) {
+                return true;
+            }
+        }
+        return gcd == 1;
+    }
+    
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
---
+++ Day-48/solution2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int xor = x ^ y;
+        int count = 0;
+        
+        while (xor != 0) {
+            count += xor & 1;
+            xor = xor >> 1;
+        }
+        return count;
+    }
+}
